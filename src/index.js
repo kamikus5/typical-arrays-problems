0b730ac0eb92153d303d3ab21572257a917c9486
Dmitriy Beloborodov <kamikus5@gmail.com>
2020-09-29 23:49:48 +0300

    feat: add function min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (!array) {
+    return 0;
+  } else if (array.length === 0) {
+    return 0;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    for (let k = 0; k < array.length; k++) {
+      let swap;
+      if (array[k] > array[k + 1]) {
+        swap = array[k];
+        array[k] = array[k + 1];
+        array[k + 1] = swap;
+      }
+    }
+  }
+  return array[0];
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (!array) {
+    return 0;
+  } else if (array.length === 0) {
+    return 0;
+  }
+
+  for (let i = 0; i <array.length; i++) {
+    for (let k = 0; k < array.length; k++) {
+      let swap;
+      if (array[k] < array[k + 1]) {
+        swap = array[k];
+        array[k] = array[k + 1];
+        array[k + 1] = swap;
+      }
+    }
+  }
+  return array[0];
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (!array) {
+    return 0;
+  } else if (array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+
+  for(let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum / array.length;
+};
